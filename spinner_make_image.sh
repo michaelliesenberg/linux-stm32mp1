200e6ab2d7dc97c037ec267925094a8ad8a3ea84
root <root@debian10.linuxvmimages.local>
2021-02-09 16:53:15 +0100

    Add Script

--- spinner_make_image.sh
+++ spinner_make_image.sh
@@ -1,5 +1,5 @@
-sudo ARCH=arm CROSS_COMPILE=/opt/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf- make spinner.config
-sudo ARCH=arm CROSS_COMPILE=/opt/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf- make dtbs
-sudo ARCH=arm CROSS_COMPILE=/opt/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf- make zImage
-sudo ARCH=arm CROSS_COMPILE=/opt/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf- make modules
-sudo ARCH=arm CROSS_COMPILE=/opt/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf- make INSTALL_MOD_PATH="/home/debian/work/ROOTFS_STM" modules_install
+sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make spinner.config
+sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make dtbs
+sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make zImage
+sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make modules
+sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make INSTALL_MOD_PATH="/home/debian/work/ROOTFS_STM" modules_install
