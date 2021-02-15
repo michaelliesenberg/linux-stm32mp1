323b3901bd21aabb4e002a8768e932e253872552
michaelliesenberg <michael@liesenberg.biz>
2021-02-15 10:57:43 +0100

    Change make script

--- spinner_make_image.sh
+++ spinner_make_image.sh
@@ -2,6 +2,7 @@ sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-no
 sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make dtbs
 sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make zImage
 sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make modules
+sudo rm -rf /home/debian/work/ROOTFS_STM/lib/modules/*
 sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make INSTALL_MOD_PATH="/home/debian/work/ROOTFS_STM" modules_install
 sudo cp -av arch/arm/boot/zImage /home/debian/work/bootfs
 sudo cp -av arch/arm/boot/dts/stm32mp157c-spinner.dtb /home/debian/bootfs
