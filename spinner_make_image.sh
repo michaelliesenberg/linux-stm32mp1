435f0ce52c12d6bc355d6bd133ccd718b85096be
michaelliesenberg <michael@liesenberg.biz>
2021-04-26 10:25:00 +0200

    Spinner config

--- spinner_make_image.sh
+++ spinner_make_image.sh
@@ -1,8 +1,8 @@
 sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make spinner.config
 sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make dtbs
 sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make zImage
-sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make modules
+#sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make modules
 sudo rm -rf /home/debian/work/ROOTFS_STM/lib/modules/*
-sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make INSTALL_MOD_PATH="/home/debian/work/ROOTFS_STM" modules_install
+#sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make INSTALL_MOD_PATH="/home/debian/work/ROOTFS_STM" modules_install
 sudo cp -av arch/arm/boot/zImage /home/debian/work/bootfs
 sudo cp -av arch/arm/boot/dts/stm32mp157c-spinner.dtb /home/debian/bootfs
