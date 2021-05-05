919bf53bcf07e753eba79a1469454bde96d728ab
michaelliesenberg <michael@liesenberg.biz>
2021-05-05 08:24:20 +0200

    RTC clock stm32 removed

--- spinner_make_image.sh
+++ spinner_make_image.sh
@@ -1,4 +1,7 @@
-sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make spinner.config
+#sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make savedefconfig
+#cp defconfig arch/arm/configs/spinner
+sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make clean
+sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make arch/arm/configs/spinner
 sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make dtbs
 sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make zImage
 #sudo ARCH=arm CROSS_COMPILE=/home/debian/work/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- make modules
